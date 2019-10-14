1fe97617ff5551792489e3477430016a91fc6f2a
Aishwarya Singh Gaur <47079250+yashgaur09@users.noreply.github.com>
2019-10-14 10:50:31 +0530

    Create program 1.py

---
+++ program 1.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    sum1=0
+    sum2=0
+    for i in range(3):
+        if a[i]>b[i]:
+            sum1+=1
+        elif a[i]<b[i]:
+            sum2+=1
+    return sum1,sum2
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
